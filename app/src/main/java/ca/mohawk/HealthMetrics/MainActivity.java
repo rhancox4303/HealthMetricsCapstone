60cef5d1c64b26c05adaeef528315ea1677860dd
rhancox4303 <35933532+rhancox4303@users.noreply.github.com>
2019-05-15 21:10:28 -0400

    Renamed Main Activity

--- app/src/main/java/ca/mohawk/HealthMetrics/MainActivity.java
+++ app/src/main/java/ca/mohawk/HealthMetrics/MainActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MetricViewActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
