11b8090f90c613915a9dba3cdd83905c1c6b6b62
rhancox4303 <35933532+rhancox4303@users.noreply.github.com>
2019-05-18 11:25:42 -0400

    Created DeletePhotoEntryDialog Fragment.

---
+++ app/src/main/java/ca/mohawk/HealthMetrics/DeletePhotoEntryDialog.java
@@ -0,0 +1,29 @@
+package ca.mohawk.HealthMetrics;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DeletePhotoEntryDialog extends Fragment {
+
+
+    public DeletePhotoEntryDialog() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_delete_photo_entry_dialog, container, false);
+    }
+
+}
