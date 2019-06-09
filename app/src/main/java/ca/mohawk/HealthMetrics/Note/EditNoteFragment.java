b350124d3ce3bae64102892e7bf3341252817bd5
rhancox4303 <35933532+rhancox4303@users.noreply.github.com>
2019-06-09 14:20:42 -0400

    -Added Edit Note frgament and layout. -Created View note layout.

---
+++ app/src/main/java/ca/mohawk/HealthMetrics/Note/EditNoteFragment.java
@@ -0,0 +1,30 @@
+package ca.mohawk.HealthMetrics.Note;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.mohawk.HealthMetrics.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditNoteFragment extends Fragment {
+
+
+    public EditNoteFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_note, container, false);
+    }
+
+}
