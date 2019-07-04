6670b800b415eee97f73e7f6c42358ab875f1f49
rhancox4303 <35933532+rhancox4303@users.noreply.github.com>
2019-07-04 11:58:12 -0400

    Update CreateUserFragment.java
    Added Documentation.

--- app/src/main/java/ca/mohawk/HealthMetrics/UserProfile/CreateUserFragment.java
+++ app/src/main/java/ca/mohawk/HealthMetrics/UserProfile/CreateUserFragment.java
@@ -27,7 +27,8 @@ import ca.mohawk.HealthMetrics.Models.User;
 import ca.mohawk.HealthMetrics.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * The CreateUserFragment class is an extension of the Fragment class.
+ * It is used to create the user's profile when the user opens the app for the first time.
  */
 public class CreateUserFragment extends Fragment implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
@@ -36,22 +37,28 @@ public class CreateUserFragment extends Fragment implements View.OnClickListener
         // Required empty public constructor
     }
 
-    HealthMetricsDbHelper healthMetricsDbHelper;
-    DatePickerFragment datePickerFragment = new DatePickerFragment();
+    private HealthMetricsDbHelper healthMetricsDbHelper;
+    private DatePickerFragment datePickerFragment = new DatePickerFragment();
 
-    EditText dateOfBirthEditText;
-    EditText firstNameEditText;
-    EditText lastNameEditText;
-    RadioGroup genderRadioGroup;
+    private EditText dateOfBirthEditText;
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private RadioGroup genderRadioGroup;
 
+    /**
+     * The onCreateView method initializes the view variables 
+     * and the HealthMetricsDbHelper object when the Fragment view is created.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //Hide the action bar to prevent the user from navigating to a different section of the application.
         ((MainActivity)getActivity()).getSupportActionBar().hide();
-        View rootView =  inflater.inflate(R.layout.fragment_create_user, container, false);
         getActivity().setTitle("Create User Profile");
-
+        
+        View rootView =  inflater.inflate(R.layout.fragment_create_user, container, false);
+        
         healthMetricsDbHelper = HealthMetricsDbHelper.getInstance(getActivity());
 
         dateOfBirthEditText = rootView.findViewById(R.id.editTextDateOfBirthUserCreation);
@@ -59,13 +66,17 @@ public class CreateUserFragment extends Fragment implements View.OnClickListener
         lastNameEditText = rootView.findViewById(R.id.editTextLastName);
         genderRadioGroup = rootView.findViewById(R.id.radioGroupGender);
         Button createUserButton = rootView.findViewById(R.id.buttonCreateUser);
-
+      
         createUserButton .setOnClickListener(this);
         dateOfBirthEditText.setOnClickListener(this);
 
         return rootView;
     }
-
+    
+    /**
+     * The createUser method gets the user inputed values, creates the user and uses the healthMetricsDbHelper
+     * object to add the created user to the database.
+     */
     public void createUser() {
 
         String firstName = firstNameEditText.getText().toString();
@@ -73,45 +84,62 @@ public class CreateUserFragment extends Fragment implements View.OnClickListener
         String dateOfBirth = dateOfBirthEditText.getText().toString();
         String gender;
 
-
+        //Get the user's selected gender.
         if(genderRadioGroup.getCheckedRadioButtonId() == R.id.radioButtonMale){
             gender = "Male";
         }else{
             gender = "Female";
         }
-
+        
+        //Verify the user's data entry
         if(firstName.matches("")  || lastName.matches("")| dateOfBirth.matches("") ){
             Toast.makeText(getActivity(), "Please fill in all fields.", Toast.LENGTH_SHORT).show();
         }else{
-
+            
             healthMetricsDbHelper.addUser(new User(firstName,lastName,gender,dateOfBirth));
 
             Toast.makeText(getActivity(), "User added.", Toast.LENGTH_SHORT).show();
-
-            SharedPreferences userInfo = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            SharedPreferences.Editor  editor = userInfo.edit();
-
-            editor.putBoolean("loggedIn", true);
-            editor.commit();
-
-            MetricsViewFragment metricsViewFragment = new MetricsViewFragment();
-
+            
+            storeLoggedInState();
+            
+            MetricsViewFragment metricsViewFragment = new MetricsViewFragment();        
             getActivity().getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragmentContainer,metricsViewFragment )
                     .addToBackStack(null)
                     .commit();
         }
     }
+    
+    /**
+     * The storeLoggedInState method writes true to a SharedPreference that is used to 
+     * verify if the user has created an account.
+     */
+    public void storeLoggedInState(){
+        SharedPreferences userInfo = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences.Editor editor = userInfo.edit();
 
+        editor.putBoolean("loggedIn", true);
+        editor.commit();
+    }
+    
+    /**
+     * The onDateSet method is ran when a date is selected from the datepicker dialog.
+     * The date of birth field is set to the date that was selected.
+     */
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        //Add a 0 to the day if it less than 10.
         if (dayOfMonth < 10) {
             dateOfBirthEditText.setText((month + 1) + "-0" + dayOfMonth + "-" + year);
         }else{
             dateOfBirthEditText.setText((month + 1) + "-" + dayOfMonth + "-" + year);
         }
     }
-
+    
+    /**
+     * The onClick method runs when the a view's onClickListener is activated.
+     * It runs the createUser method or opens the DatePickerFragment depending on what was clicked.
+     */
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.buttonCreateUser) {
