03d32e39283bb61b91a2dc3565828294505593d5
rhancox4303 <35933532+rhancox4303@users.noreply.github.com>
2019-07-04 11:04:32 -0400

    Update MetricRecyclerViewAdapter.java
    Added documentation.

--- app/src/main/java/ca/mohawk/HealthMetrics/Adapaters/MetricRecyclerViewAdapter.java
+++ app/src/main/java/ca/mohawk/HealthMetrics/Adapaters/MetricRecyclerViewAdapter.java
@@ -12,15 +12,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import ca.mohawk.HealthMetrics.DisplayObjects.MetricRecyclerViewObject;
 import ca.mohawk.HealthMetrics.R;
 
+/**
+ * The MetricRecyclerViewAdapter is a custom adapater to display
+ * MetricRecyclerViewObject objects in the Metrics View Recycler View.
+ */
 public class MetricRecyclerViewAdapter extends
         RecyclerView.Adapter<MetricRecyclerViewAdapter.ViewHolder>
 {
+    //The list of mtrics to be displayed in the recycler view.
     private List<MetricRecyclerViewObject> metricRecyclerViewObjectList;
-
+    
     public MetricRecyclerViewAdapter(List<MetricRecyclerViewObject> metricRecyclerViewObjectList) {
         this.metricRecyclerViewObjectList = metricRecyclerViewObjectList;
     }
-
+    
+    /**
+    * The ViewHolder class describes the item view and 
+    * metadata about it's place within the RecyclerView.
+    */
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView textViewMetricName;
@@ -33,27 +42,33 @@ public class MetricRecyclerViewAdapter extends
             latestMetricDataEntry = (TextView) itemView.findViewById(R.id.textViewMetricDataEntry);
         }
     }
-
+    
+     /**
+     * The onCreateViewHolder method is used to inflate 
+     * the custom layoutand create the viewholder.
+     */
     @Override
     public MetricRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        // Inflate the custom layout
         View contactView = inflater.inflate(R.layout.fragment_metrics_custom_recyclerview_layout, parent, false);
 
-        // Return a new holder instance
         ViewHolder viewHolder = new ViewHolder(contactView);
         return viewHolder;
     }
 
-    // Involves populating data into the item through holder
+     /**
+     * The onBindViewHolder method is used to set the item views in the recycler
+     * view using the metricRecyclerViewObjectList and the view holder.
+     */
     @Override
     public void onBindViewHolder(MetricRecyclerViewAdapter.ViewHolder viewHolder, int position) {
-        // Get the data model based on position
+        //Get data object
         MetricRecyclerViewObject metric = metricRecyclerViewObjectList.get(position);
 
-        // Set item views based on your views and data model
+        // Set item views
         TextView metricTextView = viewHolder.textViewMetricName;
         metricTextView.setText(metric.getMetricName());
         TextView latestDataEntryTextView = viewHolder.latestMetricDataEntry;
