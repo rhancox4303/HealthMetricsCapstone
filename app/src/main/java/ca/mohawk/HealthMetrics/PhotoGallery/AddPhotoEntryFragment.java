981ecf99ca5510678701aad98e1f91a4e176badd
rhancox4303 <35933532+rhancox4303@users.noreply.github.com>
2019-09-19 22:56:33 -0400

    Patchy fix to 90 degree problem. Need to Figure out full solution.

--- app/src/main/java/ca/mohawk/HealthMetrics/PhotoGallery/AddPhotoEntryFragment.java
+++ app/src/main/java/ca/mohawk/HealthMetrics/PhotoGallery/AddPhotoEntryFragment.java
@@ -1,37 +1,38 @@
 //Taken from https://developer.android.com/training/camera/photobasics
 package ca.mohawk.HealthMetrics.PhotoGallery;
 
-        import android.Manifest;
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.content.pm.PackageManager;
-        import android.graphics.Bitmap;
-        import android.net.Uri;
-        import android.os.Bundle;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Bundle;
 
-        import androidx.appcompat.app.AlertDialog;
-        import androidx.core.content.ContextCompat;
-        import androidx.core.content.FileProvider;
-        import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
-        import android.os.Environment;
-        import android.provider.MediaStore;
-        import android.util.Log;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.Button;
-        import android.widget.ImageView;
-        import android.widget.Toast;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-        import java.io.File;
-        import java.io.IOException;
-        import java.text.SimpleDateFormat;
-        import java.util.Date;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-        import ca.mohawk.HealthMetrics.R;
+import ca.mohawk.HealthMetrics.R;
 
-        import static android.app.Activity.RESULT_OK;
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -45,6 +46,7 @@ public class AddPhotoEntryFragment extends Fragment implements View.OnClickListe
     private static final int REQUEST_TAKE_PHOTO = 2000;
     private static final int CAMERA_PERMISSION_CODE = 100;
     private String currentPhotoPath;
+    private Uri currentPhotoUri;
     // private static final int GALLERY_INTENT_CODE = 2001;
     // private static final int GALLERY_PERMISSION_CODE = 101;
 
@@ -69,29 +71,6 @@ public class AddPhotoEntryFragment extends Fragment implements View.OnClickListe
         showDialog();
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                Log.d("ERROR", ex.toString());
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(getActivity(),
-                        "ca.mohawk.HealthMetrics.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 
@@ -112,23 +91,31 @@ public class AddPhotoEntryFragment extends Fragment implements View.OnClickListe
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            Matrix matrix = new Matrix();
+
+            matrix.postRotate(90);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), currentPhotoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            imageView.setImageBitmap(rotatedBitmap);
         }
     }
 
-    private void checkPermissions(){
-        if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(
                     new String[]{Manifest.permission.CAMERA},
                     CAMERA_PERMISSION_CODE);
+        } else {
+            dispatchTakePictureIntent();
         }
-        else {dispatchTakePictureIntent(); }
     }
 
-    private void showDialog(){
+    private void showDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         builder.setTitle("Insert Photo Entry");
@@ -148,6 +135,28 @@ public class AddPhotoEntryFragment extends Fragment implements View.OnClickListe
         builder.show();
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.d("ERROR", ex.toString());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                currentPhotoUri = FileProvider.getUriForFile(getActivity(),
+                        "ca.mohawk.HealthMetrics.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -161,6 +170,7 @@ public class AddPhotoEntryFragment extends Fragment implements View.OnClickListe
 
         // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
+        Log.d("PATH", currentPhotoPath);
         return image;
     }
 }
