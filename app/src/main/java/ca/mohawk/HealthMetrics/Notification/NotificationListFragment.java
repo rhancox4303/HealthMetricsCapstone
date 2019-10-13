90ee91876df4d13f2a535e15aa4e55a204aa763a
rhancox4303 <riley.hancox@mohawkcollege.ca>
2019-10-13 13:01:59 -0400

    Implemented Notification List Fragment Navigation

--- app/src/main/java/ca/mohawk/HealthMetrics/Notification/NotificationListFragment.java
+++ app/src/main/java/ca/mohawk/HealthMetrics/Notification/NotificationListFragment.java
@@ -2,19 +2,24 @@ package ca.mohawk.HealthMetrics.Notification;
 
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import ca.mohawk.HealthMetrics.HealthMetricsDbHelper;
 import ca.mohawk.HealthMetrics.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NotificationListFragment extends Fragment {
+public class NotificationListFragment extends Fragment implements View.OnClickListener {
 
+    private HealthMetricsDbHelper healthMetricsDbHelper;
 
     public NotificationListFragment() {
         // Required empty public constructor
@@ -24,8 +29,24 @@ public class NotificationListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        healthMetricsDbHelper = HealthMetricsDbHelper.getInstance(getActivity());
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_notification_list, container, false);
+        Button createNotificationButton = rootView.findViewById(R.id.buttonCreateNotificationNotificationList);
+        createNotificationButton.setOnClickListener(this);
+
+        return rootView;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        CreateNotificationFragment createNotificationFragment = new CreateNotificationFragment();
+
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, createNotificationFragment)
+                .addToBackStack(null)
+                .commit();
+    }
 }
