83c3f81b6d6f3d7057062606b81d5f942f74375c
rhancox4303 <35933532+rhancox4303@users.noreply.github.com>
2019-11-08 00:57:49 -0500

    Documented Time and date pickers

--- app/src/main/java/ca/mohawk/HealthMetrics/DatePickerFragment.java
+++ app/src/main/java/ca/mohawk/HealthMetrics/DatePickerFragment.java
@@ -33,13 +33,13 @@ public class DatePickerFragment extends DialogFragment{
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker
+
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-        // Create a new instance of DatePickerDialog and return it
+        // Return a new instance of DatePickerDialog
         return new DatePickerDialog(Objects.requireNonNull(getActivity()), onDateSetListener, year, month, day);
     }
 }
--- app/src/main/java/ca/mohawk/HealthMetrics/TimePickerFragment.java
+++ app/src/main/java/ca/mohawk/HealthMetrics/TimePickerFragment.java
@@ -10,22 +10,32 @@ import java.util.Calendar;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+/**
+ * The TimePickerFragment class is an extension of the Dialog Fragment class.
+ * Creates a standardized Time Picker Dialog used across the application.
+ */
 public class TimePickerFragment extends DialogFragment {
 
+    // Initialize the onDateSetListener.
     private TimePickerDialog.OnTimeSetListener onTimeSetListener;
 
+    /**
+     * Sets the onTimeSetListener.
+     *
+     * @param onTimeSetListener Represents the onTimeSetListener.
+     */
     public void setOnTimeSetListener(TimePickerDialog.OnTimeSetListener onTimeSetListener) {
         this.onTimeSetListener = onTimeSetListener;
     }
 
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current time as the default values for the picker
+
         final Calendar c = Calendar.getInstance();
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
 
-        // Create a new instance of TimePickerDialog and return it
+        // Return a new instance of TimePickerDialog
         return new TimePickerDialog(getActivity(), onTimeSetListener, hour, minute,
                 DateFormat.is24HourFormat(getActivity()));
     }
